d598a8d6d9879d248789d6f8cf6a36e3d0f4c2a3
yhkwak76-sys <yhkwak76@gmail.com>
2025-10-27 22:15:47 +0900

    update and  remove file

---
+++ mvp_ktds_kyh_001.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+print("OpenAI package imported successfully.")
+
+
+
+# Set endpoints and deployment model (provide the name of the deployment)
+AZURE_SEARCH_ENDPOINT = ""
+AZURE_OPENAI_ENDPOINT = ""
+AZURE_DEPLOYMENT_MODEL = "dev-gpt-4.1-mini"  # e.g., "gpt-35-turbo", "gpt-4o", etc.
+AZURE_SEARCH_API_KEY = ""
+AZURE_OPENAI_API_KEY = ""
+
+print(AZURE_DEPLOYMENT_MODEL)
